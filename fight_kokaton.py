909d8e18f8498a208c301a369720e05c3aabbfad
asami <c0b2200414@edu.teu.ac.jp>
2023-05-09 14:49:29 +0900

    練習問題１

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -108,6 +108,24 @@ class Bomb:
         screen.blit(self._img, self._rct)
 
 
+class Beam:
+    def __init__(self, bird):
+        self._img = pg.image.load(f"ex03/fig/beam.png")
+        self._rct = self._img.get_rect()
+        self._rct.centerx = bird._rct.centerx + bird._rct.width
+        self._rct.centery = bird._rct.centery
+        self._vx, self._vy = +1, 0 
+
+ 
+
+    def update(self, screen: pg.Surface):
+        self._rct.move_ip(self._vx, self._vy)
+        screen.blit(self._img, self._rct)
+
+
+
+
+
 def main():
     pg.display.set_caption("たたかえ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
@@ -116,25 +134,36 @@ def main():
 
     bird = Bird(3, (900, 400))
     bomb = Bomb((255, 0, 0), 10)
+    beam = None
 
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 return
+            if event.type == pg.KEYDOWN and event.key == pg.K_SPACE:
+                beam = Beam(bird)
         tmr += 1
         screen.blit(bg_img, [0, 0])
         
-        if bird._rct.colliderect(bomb._rct):
-            # ゲームオーバー時に，こうかとん画像を切り替え，1秒間表示させる
-            bird.change_img(8, screen)
-            pg.display.update()
-            time.sleep(1)
-            return
+        if bomb is not None:
+            if bird._rct.colliderect(bomb._rct):
+                # ゲームオーバー時に，こうかとん画像を切り替え，1秒間表示させる
+                bird.change_img(8, screen)
+                pg.display.update()
+                time.sleep(1)
+                return
 
         key_lst = pg.key.get_pressed()
         bird.update(key_lst, screen)
-        bomb.update(screen)
+        if bomb is not None:
+            bomb.update(screen)
+        if beam is not None:
+            beam.update(screen)
+            if bomb is not None:
+                if beam._rct.colliderect(bomb._rct):
+                    beam = None
+                    bomb = None
         pg.display.update()
         clock.tick(1000)
 
